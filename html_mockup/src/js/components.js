3ed0a823b7f18a22282f67ab642f49fb3489bd2c
タイン <Thanh.HC225929@sis.hust.edu.vn>
2025-03-15 11:35:24 +0700

    fix path

--- html_mockup/src/js/components.js
+++ html_mockup/src/js/components.js
@@ -1,19 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function loadComponent(selector, file, callback) {
-      fetch(file)
-        .then((response) => response.text())
-        .then((data) => {
-          document.querySelector(selector).innerHTML = data;
-          if (callback) callback();
-        })
-        .catch((error) => console.error("Error loading component:", error));
-    }
-  
-  loadComponent("#navbar", "https://thanhchiyeukurumi.github.io/intern_project/html_mockup/src/components/NavBar.html");
-  loadComponent('#navbar-guest', "https://thanhchiyeukurumi.github.io/intern_project/html_mockup/src/components/NavBar-guest.html");
-  loadComponent("#navbar-user", "https://thanhchiyeukurumi.github.io/intern_project/html_mockup/src/components/NavBar-user.html");
-  loadComponent("#navbar-owner", "https://thanhchiyeukurumi.github.io/intern_project/html_mockup/src/components/NavBar-user.html");
+  function loadComponent(selector, file, callback) {
+    // Kiểm tra xem selector có tồn tại không
+    const element = document.querySelector(selector);
+    if (!element) return;
+    
+    fetch(file)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then((data) => {
+        element.innerHTML = data;
+        if (callback) callback();
+      })
+      .catch((error) => console.error("Error loading component:", error));
+  }
 
-  loadComponent("#footer", "https://thanhchiyeukurumi.github.io/intern_project/html_mockup/src/components/Footer.html");
-  });
+
+  const basePath = "https://thanhchiyeukurumi.github.io/intern_project/html_mockup/src/components/";
   
+  loadComponent("#navbar", basePath + "NavBar.html");
+  loadComponent('#navbar-guest', basePath + "NavBar-guest.html");
+  loadComponent("#navbar-user", basePath + "NavBar-user.html");
+  loadComponent("#navbar-owner", basePath + "NavBar-user.html"); 
+  
+  loadComponent("#footer", basePath + "Footer.html");
+});
