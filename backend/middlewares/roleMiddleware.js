669185b867f3c9bec50776cf2d1907c4db2e673b
タイン <Thanh.HC225929@sis.hust.edu.vn>
2025-03-27 09:57:00 +0700

    little change

--- backend/middlewares/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-/**
- * Middleware kiểm tra quyền truy cập dựa trên vai trò
- * 
- * @fileoverview File này tham chiếu đến các middleware phân quyền trong kernels/middlewares/role
- * để duy trì khả năng tương thích ngược.
- */
-
-// Tái export từ kernels/middlewares/role
-module.exports = require('kernels/middlewares').role; 
