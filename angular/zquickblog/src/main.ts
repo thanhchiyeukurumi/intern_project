29238188a4dc5166102b09f3e340ab6108110214
タイン <Thanh.HC225929@sis.hust.edu.vn>
2025-05-07 18:58:14 +0700

    thêm import bị thiếu

--- angular/zquickblog/src/main.ts
+++ angular/zquickblog/src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { Chart, LinearScale, LineController, PointElement, LineElement, PieController, ArcElement } from 'chart.js';
+import { Chart, LinearScale, LineController, PointElement, LineElement, PieController, ArcElement, CategoryScale } from 'chart.js';
 
-Chart.register(LinearScale, LineController, PointElement, LineElement, PieController, ArcElement);
+Chart.register(LinearScale, LineController, PointElement, LineElement, PieController, ArcElement, CategoryScale);
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
